678629c0527d725e3202bc74943ac6ed1194d94e
Jahangir Alam <122303721+Alam040@users.noreply.github.com>
2023-03-03 01:26:59 +0600

    Updates

---
+++ static/js/617.51e27baf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkca_mark=self.webpackChunkca_mark||[]).push([[617],{8617:function(t,r,a){a.d(r,{Zuw:function(){return e}});var n=a(9983);function e(t){return(0,n.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"}}]})(t)}}}]);
+//# sourceMappingURL=617.51e27baf.chunk.js.map
